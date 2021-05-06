7dd3c2c2884af7a7fb782a45f22272326ee0f3da
Ryan Anderson <ryan0425@msn.com>
2021-05-05 22:39:15 -0700

    udpated server.js with mongoose

--- server.js
+++ server.js
@@ -1,8 +1,16 @@
 const express = require("express");
-
+const mongoose = require("mongoose");
 
 const PORT = process.env.PORT || 3000;
 const app = express();
+
+mongoose.connect(
+    process.env.MONGODB_URI || "mongodb://localhost/Workout_Tracker",
+    {
+        useNewUrlParser: true,
+    }
+);
+
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
@@ -12,4 +20,3 @@ app.use(express.static('public'));
 app.listen(PORT, () => {
     console.log(`App running on port ${PORT}!`)
 });
-
