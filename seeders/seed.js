76a78986af877e2e4c6d1d1a4be3e7d5f1fd9cd8
Ryan Anderson <ryan0425@msn.com>
2021-05-07 15:44:59 -0700

    updated seed.js

--- seeders/seed.js
+++ seeders/seed.js
@@ -6,6 +6,7 @@ mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", {
   useFindAndModify: false
 });
 
+
 let workoutSeed = [
   {
     day: new Date().setDate(new Date().getDate() - 10),
