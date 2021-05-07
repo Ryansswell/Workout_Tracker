ae8dbe2bc6fe7c9b3da1686106ac1f5ae02528db
Ryan Anderson <ryan0425@msn.com>
2021-05-07 14:48:41 -0700

    deploying

--- server.js
+++ server.js
@@ -5,11 +5,15 @@ const PORT = process.env.PORT || 3000;
 const app = express();
 
 mongoose.connect(
-    process.env.MONGODB_URI || "mongodb://localhost/Workout_Tracker",
+    process.env.MONGODB_URI || 'mongodb://localhost/workout',
     {
         useNewUrlParser: true,
+        useUnifiedTopology: true,
+        useCreateIndex: true,
+        useFindAndModify: false,
     }
 );
+
 
 
 app.use(express.urlencoded({ extended: true }));
