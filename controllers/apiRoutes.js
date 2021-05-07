a1442e80264abfafc91f7108f022e1cc281e133b
Ryan Anderson <ryan0425@msn.com>
2021-05-07 15:44:01 -0700

    apiRoutes updates

--- controllers/apiRoutes.js
+++ controllers/apiRoutes.js
@@ -1,8 +1,23 @@
 const router = require("express").Router();
+const db = require("../models");
 
-router.get("/notes", (req, res) => {
-    de.Note.find({})
-        .then((dbNote)) => {
 
+router.get("/workouts", async (req, res) => {
+    try {
+        const lastWorkout = await db.Workout.find({});
+        res.status(200).json(lastWorkout);
+    } catch (error) {
+        res.status(500);
+    }
+});
+
+
+router.get("/workouts/range", async (req, res) => {
+    try {
+        const lastWorkout = await db.Workout.find({});
+        const goBack = await db.goBack.find({});
+        res.status(200).json(lastWorkout);
+    } catch (error) {
+        res.status(500);
     }
 });
--- seeders/seed.js
+++ seeders/seed.js
@@ -1,14 +1,14 @@
 let mongoose = require("mongoose");
 let db = require("../models");
 
-mongoose.connect("mongodb://localhost/workout", {
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", {
   useNewUrlParser: true,
   useFindAndModify: false
 });
 
 let workoutSeed = [
   {
-    day: new Date().setDate(new Date().getDate()-10),
+    day: new Date().setDate(new Date().getDate() - 10),
     exercises: [
       {
         type: "resistance",
@@ -21,7 +21,7 @@ let workoutSeed = [
     ]
   },
   {
-    day: new Date().setDate(new Date().getDate()-9),
+    day: new Date().setDate(new Date().getDate() - 9),
     exercises: [
       {
         type: "resistance",
@@ -34,7 +34,7 @@ let workoutSeed = [
     ]
   },
   {
-    day: new Date().setDate(new Date().getDate()-8),
+    day: new Date().setDate(new Date().getDate() - 8),
     exercises: [
       {
         type: "resistance",
@@ -47,7 +47,7 @@ let workoutSeed = [
     ]
   },
   {
-    day: new Date().setDate(new Date().getDate()-7),
+    day: new Date().setDate(new Date().getDate() - 7),
     exercises: [
       {
         type: "cardio",
@@ -58,7 +58,7 @@ let workoutSeed = [
     ]
   },
   {
-    day: new Date().setDate(new Date().getDate()-6),
+    day: new Date().setDate(new Date().getDate() - 6),
     exercises: [
       {
         type: "resistance",
@@ -71,7 +71,7 @@ let workoutSeed = [
     ]
   },
   {
-    day: new Date().setDate(new Date().getDate()-5),
+    day: new Date().setDate(new Date().getDate() - 5),
     exercises: [
       {
         type: "resistance",
