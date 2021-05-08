d99a3355e63a03eb7defd692e7f0c40370a7399b
Ryan Anderson <ryan0425@msn.com>
2021-05-07 19:18:21 -0700

    updated the homeRoutes

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -1,8 +1,17 @@
 const router = require("express").Router();
 const path = require("path");
 
-router.get("/notes", (req, res) => {
-    res.sendFile(path.join(__dirname, "../public/notes.html"));
+router.get("/", (req, res) => {
+    res.sendFile(path.join(__dirname, "../public/index.html"));
+});
+
+
+router.get("/stats", (req, res) => {
+    res.sendFile(path.join(__dirname, "../public/stats.html"));
+});
+
+router.get("/exercise", (req, res) => {
+    res.sendFile(path.join(__dirname, "../public/exercise.html"));
 });
 
 module.exports = router;
