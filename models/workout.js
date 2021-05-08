42c8a8a110bf61d8d7039cae2f014298b5644877
Ryan Anderson <ryan0425@msn.com>
2021-05-07 19:15:45 -0700

    updated index.js with routes

--- models/workout.js
+++ models/workout.js
@@ -1,5 +1,38 @@
-
+const mongoose = require("mongoose");
 
 const { Schema } = mongoose;
 
-const workoutSchema = new Schema({})
+const workoutSchema = new Schema({
+    day: {
+        type: Date,
+        default: Date.now,
+    },
+    exercises: [
+        {
+            type: {
+                type: String,
+                trim: true,
+                required: true
+            },
+            duration: {
+                type: Number,
+                required: true
+            },
+            name: {
+                type: String,
+                trim: true,
+                required: true
+
+            },
+            weight: Number,
+            reps: Number,
+            sets: Number,
+            distance: Number
+        },
+    ],
+});
+
+
+const Workout = mongoose.model("Workout", workoutSchema);
+
+module.exports = Workout;
